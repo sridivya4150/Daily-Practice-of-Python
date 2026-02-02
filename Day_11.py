a01a19dbbea858169f05a6161b13d3845797251e
sridivya <sridivyasetty9@gmail.com>
2026-02-02 22:49:46 +0530

    python conditional statements

---
+++ Day_11.py
@@ -0,0 +1,6 @@
+#conditional statements in python
+#if conditional statement
+#if statement is the simplest form of a conditional statement.it executes a block of code if the given condition is true
+age=20
+if age>=18:
+    print("eligible to vote")
