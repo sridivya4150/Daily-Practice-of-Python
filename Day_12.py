e5957eeed83cca78f39e5e1f3166e9523ad8cc4b
sridivya <sridivyasetty9@gmail.com>
2026-02-05 23:27:49 +0530

    python loops

---
+++ Day_12.py
@@ -0,0 +1,9 @@
+#python loops-while,for,for each
+#while-it execute a set of statements as long as a condition is true
+i=1
+while i>5:
+    print(i)
+    i+=1
+#if we dont increment i or else the loop will continue forever
+#break statement-by using break statement we can stop the loop even if the while condition is true
+
