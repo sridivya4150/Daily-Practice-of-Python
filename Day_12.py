407137a08bbb2c4e192ae67472a8294d341ce615
sridivya <sridivyasetty9@gmail.com>
2026-02-06 23:04:21 +0530

    python loops

--- Day_12.py
+++ Day_12.py
@@ -6,4 +6,35 @@ while i>5:
     i+=1
 #if we dont increment i or else the loop will continue forever
 #break statement-by using break statement we can stop the loop even if the while condition is true
+i=0
+a="i love learning python"
+while i<len(a):
+    if a[i]=='e' or a[i]=='s':
+        i +=1
+        break
+    print(a[i])
+    i +=1
+#continue statement--by using continue statement we can stop the current iteration,and continue with the next
+i=0
+while i<6:
+    i += 1
+    if i==3:
+        continue
+    print(i)
+#while loop with else--it is executed only when the loop finishes normally
+#if the loop is terminated using a break statement,the else block does not execute
+a=0 
+while a<5:
+    a+=1
+    print(a)
+else:
+    print("loop ended naturally")
+while a<6:
+    a +=1
+    print(a)
+    break
+else:
+    print("loop not ended naturally")
+
+
 
