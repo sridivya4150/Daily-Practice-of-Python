bc486b17d69f13ba75db6ed33bb5a7002c8e0642
sridivya <sridivyasetty9@gmail.com>
2026-02-11 22:19:29 +0530

    examples for loops

--- Day_12examples.py
+++ Day_12examples.py
@@ -4,4 +4,8 @@ for i in range(rows):
     for j in range(i+1):
         print("*",end=" ")
     print()
+for i in range(rows,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
     
