3b76389561ca9ea12ef1a86856e306ccd2aa90fc
sridivya <sridivyasetty9@gmail.com>
2026-02-16 21:53:55 +0530

    python functions

---
+++ Day_13.py
@@ -0,0 +1,8 @@
+#function-a function is a reusable block of code or programming statements designed to perform a certain task
+#to define function or declare function we use def keyword
+def addition():
+    a=2
+    b=3
+    sum=a+b
+    print(sum)
+addition()
